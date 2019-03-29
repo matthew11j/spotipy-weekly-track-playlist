46ce011c3f7e1e96fabefbdd4ca030eac32e2c00
matthew11j <jacks3mh@mail.uc.edu>
2019-03-29 09:15:48 -0400

    artist fix

--- weekly-track-playlist.py
+++ weekly-track-playlist.py
@@ -16,10 +16,11 @@ def getArtistIds(names):
     artist_ids = []
     for artist_name in artist_names:
         artist = spotifyObject.search(q='artist:' + artist_name, type='artist')
-        artist_id = artist['artists']['items'][0]['id']
-        artist_ids.append(artist_id)
+        artistObject = artist['artists']['items'][0]
+        if int(artistObject['popularity']) > 50:
+            artist_id = artistObject['id']
+            artist_ids.append(artist_id)
     return artist_ids
-
 
 # Returns today's date minus 14 days
 def getDateLimit():
@@ -90,48 +91,50 @@ def replaceTracks(tracks):
 
 
 # -----------------------------------------------------------------------------
-artist_names = ['Juice WRLD', 'A$AP Rocky', 'XXXTentacion', 'Tyler, The Creator', 'Frank Ocean', 'Khalid', 'Logic']
+artist_names = ['Juice WRLD', 'A$AP Rocky', 'XXXTentacion', 'Tyler, The Creator', 'Frank Ocean', 'Khalid', 'Logic', 'Billie Eilish']
 artist_names.extend(['Travis Scott', 'Kayne West', 'Kid Cudi', 'Lil Skies', 'Ski Mask The Slump God', 'Drake', 'Post Malone', 'Jaden Smith'])
 todaysDate = datetime.today().date()
 load_config()
 tracksToAdd = []
 scope = 'user-top-read user-read-private playlist-modify-private playlist-modify-public'
 token = util.prompt_for_user_token(user_config['username'], scope=scope, client_id=user_config['client_id'], client_secret=user_config['client_secret'], redirect_uri=user_config['redirect_uri'])
-spotifyObject = spotipy.Spotify(auth=token)
+if token:
+    spotifyObject = spotipy.Spotify(auth=token)
+    artist_ids = getArtistIds(artist_names)
+    for artist_id in artist_ids:
+        checkTracks = []
+        checkedTracks = []
+        checkAlbums = []
+        items = getNewAlbumsForArtist(artist_id)
+        for item in items:
+            if item['type'] is 'album':
+                checkAlbums.append(item)
+            else:
+                checkTracks.append(item)
+        albums = Remove(checkAlbums)
+        checkedTracks = Remove(checkTracks)
+        for track in checkedTracks:
+            tracksToAdd.append(track['id'])
+        artistTracks = []
+        for album in albums:
+            album_id = album['id']
+            newTracks = getTracksByAlbumId(album_id)
+            newTracksList = newTracks['items']
+            trackUris = []
+            for track in newTracksList:
+                track_uri = track['uri']
+                trackUris.append(track_uri)
+            artistTracks.extend(trackUris)
+        tracksToAdd.extend(artistTracks)
 
-artist_ids = getArtistIds(artist_names)
-for artist_id in artist_ids:
-    checkTracks = []
-    checkedTracks = []
-    checkAlbums = []
-    items = getNewAlbumsForArtist(artist_id)
-    for item in items:
-        if item['type'] is 'album':
-            checkAlbums.append(item)
-        else:
-            checkTracks.append(item)
-    albums = Remove(checkAlbums)
-    checkedTracks = Remove(checkTracks)
-    for track in checkedTracks:
-        tracksToAdd.append(track['id'])
-    artistTracks = []
-    for album in albums:
-        album_id = album['id']
-        newTracks = getTracksByAlbumId(album_id)
-        newTracksList = newTracks['items']
-        trackUris = []
-        for track in newTracksList:
-            track_uri = track['uri']
-            trackUris.append(track_uri)
-        artistTracks.extend(trackUris)
-    tracksToAdd.extend(artistTracks)
-
-artistString = ', '.join(map(str, artist_names))
-detailsDate = datetime.strptime(todaysDate.strftime('%Y/%m/%d'),'%Y/%m/%d').strftime('%m/%d/%Y')
-playlistName = 'New Music for ' + detailsDate
-description = '***** Updated ' + detailsDate + ' ***** Artist\'s included in this week\'s search: ' + artistString
-print(datetime.today())
-print(description)
-replaceTracks(tracksToAdd)
-print('Success!')
-#print(json.dumps(tracks, indent=2, sort_keys=True))
+    artistString = ', '.join(map(str, artist_names))
+    detailsDate = datetime.strptime(todaysDate.strftime('%Y/%m/%d'),'%Y/%m/%d').strftime('%m/%d/%Y')
+    playlistName = 'New Music for ' + detailsDate
+    description = '***** Updated ' + detailsDate + ' ***** Artist\'s included in this week\'s search: ' + artistString
+    print(datetime.today())
+    print(description)
+    replaceTracks(tracksToAdd)
+    print('Success!')
+    #print(json.dumps(tracks, indent=2, sort_keys=True))
+else:
+    print("No Token found.")
